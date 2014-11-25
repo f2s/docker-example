d7e32ddd04ebd81a8c81671605b46722ea777f52
David <damccue@deloitte.co.uk>
2014-11-25 15:59:20 +0000

    cosmetic alterations

--- data/php/index.php
+++ data/php/index.php
@@ -1,12 +1,18 @@
 <?php
 
-echo "<b>App Container hostname:</b> " . gethostname() . "<br>\n";
-echo "Nginx (reverse proxy) directing to Apache PHP (application) container reading from a mysql (database) container attached using docker links<br><br>\n";
 
 $link = mysql_connect('mysql', 'test', 'test')
         or die ('Could not connect: ' . mysql_error());
 mysql_select_db('test')
         or die('Could not select database');
+
+echo "<b>App Container hostname:</b> " . gethostname() . "<br>\n";
+$query = 'select @@hostname';
+$result = mysql_query($query) 
+          or die ("Query failed: " . mysql_error());
+echo "<b>DB Container hostname:</b> " . implode(',',mysql_fetch_assoc($result)) . "<br>\n";
+mysql_free_result($result);
+echo "Nginx (reverse proxy) directing to Apache PHP (application) container reading from a mysql (database) container attached using docker links<br><br>\n";
 
 $query = 'insert into my_table values ( "page_view", now() )';
 $result = mysql_query($query) 
